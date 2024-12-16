4bc2f4b255f6904ce8988d597a3bd57bdb3101b3
trexi <vartypeez@gmail.com>
2024-12-16 21:33:56 +0300

    Removed previous Lab #1

--- Lab1/Task10.py
+++
@@ -1,8 +0,0 @@
-# Напишите программу, которая выводит числа от N до 1.
-
-def printReversedCountdown(n):
-	for i in range(n, 0, -1):
-		print(i)
-
-n = 5
-printReversedCountdown(n)
--- Lab1/Task8.py
+++
@@ -1,9 +0,0 @@
-# Дан кортеж чисел. Удалите все элементы, которые делятся на 3 без остатка.
-
-def deleteTupleElements(elem):
-	result = tuple(x for x in elem if x % 3 != 0)
-	return result
-
-numbers = (1,3,4,6,7,9,12,14)
-
-print(f'Result: {deleteTupleElements(numbers)}')					
